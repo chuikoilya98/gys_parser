6eea7599254cebeb35f3ee153653b6c17a6396d8
chuikoilya98 <49401081+chuikoilya98@users.noreply.github.com>
2022-01-19 17:53:19 +0500

    Add files via upload

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import sqlite3
 from datetime import datetime
 import os.path as pt
 import json
+from unittest import result
 
 #conn = sqlite3.connect(pt.abspath("database.db"))
 
@@ -16,29 +17,51 @@ class Database():
 
     def createItemsFromDict(self, items: dict) -> None:
         conn = sqlite3.connect(pt.abspath("database.db"))
+        # TODO: есть проблема с наличием символа ' в названии -это вызывает ошибку
         cursor = conn.cursor()
-        for item in items['items']:
-            dateAdd = str(datetime.now())
-            query = f"""
-         INSERT INTO sneakers
-         VALUES ('{item['source']}', {item['id']}, {item['name']}, {item['brand']}, {item['price']}, {item['oldPrice']}, {item['image']}, {item['sizes']}, 1, N , 0 , {dateAdd})
-         """
-            cursor.execute(query)
-            conn.commit()
+        actualIds = []
+        query = """
+        SELECT * FROM sneakers
+        """
+        cursor.execute(query)
+        ids = cursor.fetchall()
+        for item in ids:
+            actualIds.append(item[1])
 
-    def getNonExecutedItems(self):
+        for item in items['items']:
+            if item['id'] in actualIds:
+                continue
+            else:
+                dateAdd = str(datetime.now())
+                sizes = ''
+                for size in item['sizes']:
+                    sizes += size
+                    sizes += ', '
+                query = f"""
+            INSERT INTO sneakers
+            VALUES ('{item['source']}', '{item['id']}', '{item['name']}', '{item['brand']}', '{item['price']}', '{item['oldPrice']}', '{item['image']}', '{sizes}', '1', 'N' , '0' , '{dateAdd}')
+            """
+                cursor.execute(query)
+                conn.commit()
+
+    def getNonExecutedItems(self, count=10, brand=None) -> dict:
         conn = sqlite3.connect(pt.abspath("database.db"))
         cursor = conn.cursor()
         query = """
-        SELECT * FROM sneakers 
+        SELECT * FROM sneakers WHERE executed = 'N'
         """
         cursor.execute(query)
-        print(cursor.fetchall())
+        data = cursor.fetchall()
+        result = []
+
+        for i in range(count):
+            result.append(data[i])
+        print(result)
 
 
 k = Database()
 
 with open(pt.abspath('test.json'), 'r') as file:
     data = json.load(file)
-    k.createItemsFromDict(items=data)
+    # k.createItemsFromDict(items=data)
     k.getNonExecutedItems()
